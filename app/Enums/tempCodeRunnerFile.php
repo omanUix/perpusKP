45e68121cc1bb2ebbc70756aa3f3d0af98662afd
omanUix <rahmanabdu702@gmail.com>
2025-03-07 13:52:12 +0700

    Installasi Enums dan Migration User

---
+++ app/Enums/tempCodeRunnerFile.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace App\Enums;
+
+    enum MessageType: string
+    {
+        case CREATED = "Berhasil Menambahkan";
+
+        case UPDATED = "Berhasil Memperbarui";
+
+        case DELETED = "Berhasil Menghapus";
+
+        case ERROR = "Terjadi kesalahan. Silahkan coba lagi";
+
+        public function message(string $entity = '', ?string $error = null): string
+        {
+            if ($this === MessageType::ERROR && $error) {
+                return "{$this->value} {$error}";
+            }
+
+            return "{$this->value} {$entity}";
+        }
+    }
+
+    $message = MessageType::CREATED->message('Buku');
+    echo $message . PHP_EOL;
+    $message = MessageType::UPDATED->message('Buku');
+    echo $message . PHP_EOL;
+
+    $message = MessageType::DELETED->message('Buku');
+    echo $message . PHP_EOL;
+
+?>
