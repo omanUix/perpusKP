533249af8a86d70aff0ebc45c588ee970a09e994
ittierblue <ittierblue@gmail.com>
2025-03-08 23:15:59 +0700

    style: Refactoring Page Verify Email

--- resources/js/Pages/Auth/ResetPassword.jsx
+++ resources/js/Pages/Auth/ResetPassword.jsx
@@ -1,7 +1,4 @@
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
 import { Button } from '@/Components/ui/button';
 import { Input } from '@/Components/ui/input';
 import { Label } from '@/Components/ui/label';
@@ -39,68 +36,62 @@ export default function ResetPassword({ token, email }) {
                             </div>
                             {/* form */}
                             <form onSubmit={onHandleSubmit}>
-                                <div className='grid gap-4'>
-                                <div className='grid gap-2'>
-                                    <Label htmlFor="email">Email</Label>
+                                <div className="grid gap-4">
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="email">Email</Label>
 
-                                    <Input
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                        value={data.email}
-                                        autoComplete="username"
-                                        onChange={(e) => setData('email', e.target.value)}
-                                    />
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            name="email"
+                                            value={data.email}
+                                            autoComplete="username"
+                                            onChange={(e) => setData('email', e.target.value)}
+                                        />
 
-                                    {errors.email && ( <InputError message={errors.email} />
-                                )}
+                                        {errors.email && <InputError message={errors.email} />}
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label htmlFor="password">Password</Label>
+
+                                        <Input
+                                            id="password"
+                                            type="password"
+                                            name="password"
+                                            value={data.password}
+                                            autoComplete="new-password"
+                                            onChange={(e) => setData('password', e.target.value)}
+                                        />
+
+                                        {errors.password && <InputError message={errors.password} />}
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label htmlFor="password_confirmation">Konfirmasi Password</Label>
+
+                                        <Input
+                                            type="password"
+                                            id="password_confirmation"
+                                            name="password_confirmation"
+                                            value={data.password_confirmation}
+                                            autoComplete="new-password"
+                                            onChange={(e) => setData('password_confirmation', e.target.value)}
+                                        />
+
+                                        {errors.password_confirmation && (
+                                            <InputError message={errors.password_confirmation} className="mt-2" />
+                                        )}
+                                    </div>
+
+                                    <Button
+                                        type="submit"
+                                        variant="orange"
+                                        size="xl"
+                                        className="w-full"
+                                        disabled={processing}
+                                    >
+                                        Reset Password
+                                    </Button>
                                 </div>
-                                <div className="mt-4">
-                                    <Label htmlFor="password">Password</Label>
-
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        name="password"
-                                        value={data.password}
-                                        autoComplete="new-password"
-                                        onChange={(e) => setData('password', e.target.value)}
-                                    />
-
-                                {errors.password && ( <InputError message={errors.password} />
-                            )}
-                                </div>
-                                <div className="mt-4">
-                                    <Label htmlFor="password_confirmation">Konfirmasi Password</Label>
-
-                                    <Input
-                                        type="password"
-                                        id="password_confirmation"
-                                        name="password_confirmation"
-                                        value={data.password_confirmation}
-                                        autoComplete="new-password"
-                                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                                    />
-
-                                    {errors.password_confirmation && (  
-                                        <InputError message={errors.password_confirmation} className="mt-2" />
-                                )}
-                                </div>
-                                
-                                <Button
-                                type="submit"
-                                variant="orange"
-                                size="xl"
-                                className="w-full"
-                                disabled={processing}
-                                >
-                                    Reset Password
-                                </Button>
-                                </div>
-
-
-
-
                             </form>
                         </div>
                     </div>
